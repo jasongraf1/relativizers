f5038970740e43b2f2b3b3a6e1d69bbe4250d067
jasongraf1 <jason.grafmiller@gmail.com>
2016-10-10 14:43:24 +0200

    clean up

--- R/FAMD_analysis.R
+++ R/FAMD_analysis.R
@@ -1,16 +1,23 @@
 ##############################################################################
-# Factor Analysis of Mixed Data of English relativizers 
+#
+# Factor Analysis of Mixed Data of English relativizers
+# 
 # Jason Grafmiller
 ##############################################################################
 
 library(FactoMineR)
 
-# DATA ------------------------------------------------------------------
+
+# Load data -------------------------------------------------------------
+
 source("R/data_setup.R")
 
 #--- 2 dataframes ---
 # subjrel: the data from subject RCs
 # objrel: the data from non=subject (object) RCs
+
+
+# Functions -------------------------------------------------------------
 
 # function for displaying
 display.dims <- function(x) {
@@ -30,31 +37,29 @@ display.dims <- function(x) {
 # 1. subject FAMD
 # 2 non-subject FAMD
 
-# include the "stylistic" factors as well as the prescriptive factors 
-names(subjrel)
-
 # factors included:
 style <- names(subjrel)[c(7, 25:29,31:32, 34:37)]
 
-#=== subject RCs =====================================
+# calculate condition number of matrix
+fmd.1.cnum <- languageR::collin.fnc(as.numeric2(subjrel[, style]))$cnumber
+# 108.6358
+
+#=== subject RCs ===
 
 fmd.1 <- FAMD(subjrel[, style], ncp = length(style))
 
-summary(fmd.1)
-
 #--- Eigenvalues ---
 
-# include Dims 1-7. Only use those dimensions that account for 5% of variance 
-# or more
+# Inspect the contributions of the variables and dimensions.
+# Only use those dimensions that account for 5% of variance or more.
 round(fmd.1$eig, 2)
-
-# inspect the contributions of the variables
 round(fmd.1$var$contrib, 2)
 round(fmd.1$var$cos2, 4)
 
 #--- Correlations of the dimensions ---
 
-# use dimdesc function
+# Use dimdesc function to inspect correlations of dimensions and original
+# variables.
 dd1 <- dimdesc(fmd.1, axes = 1:7)
 
 dd1[[1]]
@@ -71,24 +76,32 @@ dd1[[2]]
 # high TTR and nouniness, press genre
 # dimension 2 is about elaborated, yet informationally sparse, style
 
+# loop through and display all the dimensions
+for(i in 1:7){
+	d <- dd1[[i]]
+	print(paste("Styistic Dimension", i))
+	for(j in 1:3){
+		print(round(d[[j]], 3))
+	}
+	cat('\n')
+}
+
 #=== object RCs =====================================
 
 fmd.2 <- FAMD(objrel[, style], ncp = length(style))
-summary(fmd.2)
+
+# Calculate condition number of matrix.
+fmd.2.cnum <- languageR::collin.fnc(as.numeric2(objrel[, style[-1]]))$cnumber
+# 130.0957
 
 #--- Eigenvalues ---
 fmd.2$eig
-# inspect the contributions of the variables
 round(fmd.2$var$contrib, 2)
+round(fmd.2$var$cos2, 4)
 
 #--- Correlations of the dimensions ---
-# include Dims 1-7. Only use those dimensions that account for 5% of variance 
-# or more
+
 dd2 <- dimdesc(fmd.2, axes = 1:7)
-
-dd2[[1]] # noun-related factors
-
-dd2[[2]] # sentence complexity
 
 # loop through and display all the dimensions
 for(i in 1:7){
@@ -100,10 +113,22 @@ for(i in 1:7){
 	cat('\n')
 }
 
+
 # INTERNAL FACTORS ------------------------------------------------------
 
 names(subjrel) # same as objrel
-internal <- names(subjrel)[c(22:23, 12, 14, 40:42, 18:20)]
+# Fix names for transparency:
+names(subjrel)[c(12, 19, 20, 22, 23, 42, 43, 18)] <- c(
+	'prior.Rel', 'ant.Given', 'ant.Definite',
+	'ant.to.RC.dist', 'RC.length',
+	'ant.Number', 'ant.POS', 'ant.length')
+names(objrel)[c(12, 19, 20, 22, 23, 42, 43, 18)] <- c(
+	'prior.Rel', 'ant.Given', 'ant.Definite',
+	'ant.to.RC.dist', 'RC.length',
+	'ant.Number', 'ant.POS', 'ant.length')
+
+internal <- names(subjrel)[c(22:23, 12, 14, 40, 42, 43, 18:20)]
+
 
 #--- Relabel category levels for interpretation ---
 
@@ -111,18 +136,24 @@ internal <- names(subjrel)[c(22:23, 12, 14, 40:42, 18:20)]
 levels(subjrel$nested) <- c("non-nested", "nested")
 levels(objrel$nested) <- c("non-nested", "nested")
 
-# antecedent POS
-levels(subjrel$antPOS2) <- c("Ant.noun", "Ant.non-noun")
-levels(objrel$antPOS2) <- c("Ant.noun", "Ant.non-noun")
+# givenness
+levels(subjrel$ant.Given) <- c("new", "given")
+levels(objrel$ant.Given) <- c("new", "given")
 
-# antecedent number
-levels(subjrel$antNum2)[1] <- "Ant.other.num"
-levels(objrel$antNum2)[1] <- "Ant.other.num"
-
+# persistence
+levels(subjrel$prior.Rel) <- c('prior=none', 'prior=THAT',
+															 'prior=WHICH', 'prior=ZERO')
+levels(objrel$prior.Rel) <- c('prior=none', 'prior=THAT',
+															 'prior=WHICH', 'prior=ZERO')
 
 #=== subject RCs =====================================
+
 fmd.3 <- FAMD(subjrel[, internal], ncp = length(internal))
 summary(fmd.3)
+
+# calculate condition number of matrix
+fmd.3.cnum <- languageR::collin.fnc(as.numeric2(subjrel[, internal[c(1,2,8)]]))$cnumber
+
 
 #--- Eigenvalues ---
 fmd.3$eig # include all
@@ -132,44 +163,26 @@ round(fmd.3$var$contrib, 2)
 # look at associations of the dimensions
 dd3 <- dimdesc(fmd.3, axes = 1:9)
 
-# (I'm doing only the dims with big effects)
-dd3[[1]]
-# +
-# distance of antecedent to RC (antH2Rc, antLN) & non adjacent
+# loop through and display all the dimensions
+for(i in 1:7){
+	d <- dd3[[i]]
+	print(paste("Styistic Dimension", i))
+	for(j in 1:3){
+		print(round(d[[j]], 3))
+	}
+	cat('\n')
+	rm(d)
+}
 
-dd3[[2]]
-# +
-# properties of the antecedent (num = other & pers = other; EMPTY heads) 
-# -
-# antPOS = noun, antNum = singular or plural
-
-dd3[[3]]
-# + 
-# RC length & nested = Y & precRel = which
-
-dd3[[4]]
-# mixed bag
-# +
-# precRel = ZERO & antNum = singular
-# -
-# precRel = that & antNum = plural
-
-dd3[[7]]
-# +
-# precRel = ZERO
-# - 
-# prec rel = that
-
-dd3[[9]]
-# +
-# RC length & nested = no
-# -
-# nested = yes
 
 #=== object RCs =====================================
 
 fmd.4 <- FAMD(objrel[, internal], ncp = length(internal))
 summary(fmd.4)
+
+# calculate condition number of matrix
+fmd.4.cnum <- languageR::collin.fnc(as.numeric2(objrel[, internal]))$cnumber
+# 44.27849
 
 #--- Eigenvalues ---
 fmd.4$eig # include all
@@ -177,24 +190,6 @@ round(fmd.4$var$contrib, 2)
 
 #--- Correlations of the dimensions ---
 dd4 <- dimdesc(fmd.4, axes = 1:9)
-
-dd4[[1]] # distance from ant to RC
-
-dd4[[2]] # properties of the ant [non-EMPTY heads]
-
-dd4[[3]] # RClength and nestedness and prec rel = WHICH
-
-dd4[[4]] # preceding rel
-
-dd4[[5]] # preceding rel
-
-dd4[[6]] # antecedent number and short RCs
-
-dd4[[7]] # preceding rel and nestedness
-
-dd4[[8]] # nestedness and RC length, and indefinite ants
-
-dd4[[9]] # adjacent RC and long RC
 
 # loop through and display all:
 for(i in 1:9){
@@ -204,6 +199,7 @@ for(i in 1:9){
 		print(round(d[[j]], 3))
 	}
 	cat('\n')
+	rm(d)
 }
 
 # ADD TO DATAFRAMES -----------------------------------------------------
@@ -226,7 +222,10 @@ objrel <- cbind(objrel, obj.int.dims)
 
 
 # ALL PREDCITORS COMBINED -----------------------------------------------
-# Just for fun, run two more FAMDs with all the predictors combined
+
+# Run two more FAMDs with all the predictors combined. This was not included
+# in the Grafmiller et al. paper
+
 n.vars <- length(c(style, internal)) 
 
 #=== subject RCs =====================================
@@ -240,29 +239,6 @@ round(fmd.5$var$contrib, 2)
 
 #--- Correlations of the dimensions ---
 dd5 <- dimdesc(fmd.5, axes = 1:21)
-
-dd5[[1]]
-# +
-# noun-related style measures (nouniness, word length) & learned genre 
-# -
-# personal pronouns & fiction
-
-dd5[[2]]
-# clause-related style measures: passives, subordConj & genre
-
-dd5[[3]]
-# antecedent distance measures:
-
-dd5[[4]]
-# properties of antecedent
-
-dd5[[5]]
-
-dd5[[7]]
-# +
-# RC length
-
-# and so on...
 
 #-- add to dataframe ---
 subj.5.dims <- fmd.5$ind$coord[, 1:17]
@@ -279,41 +255,6 @@ round(fmd.6$var$contrib,3)
 #--- Eigenvalues ---
 dd6 <- dimdesc(fmd.6, axes = 1:21)
 
-dd6[[1]] # information density
-# +
-# non-academic , informational style (nouniness, word len, also passives and sent len) + general prose & learned
-# - 
-# personal pronouns & fiction
-
-dd6[[2]] # clause complexity
-# +
-# clausal complexity (subordConj, passives, sent length) & learned
-# -
-# information density (TTR, nouniness) & newspper
-
-dd6[[3]] # distance from ant to RC
-# + 
-# antHeadToRC, antLN, non-adjacent
-
-dd6[[4]] # atypical antecedents
-# +
-# antNum2 = "other", antPOS = "other", indef
-# -
-# antPOS = "noun"
-
-dd6[[5]]
-
-dd6[[6]] # RC length and nested = yes
-
-dd6[[7]] # precRel = that, split Infitives
-
-dd6[[11]]
-
-dd6[[12]] # split Inf
-
-dd6[[13]] # precRel = ZERO
-
-dd6[[17]]
 
 #--- add to dataframe ----
 obj.5.dims <- fmd.6$ind$coord[, 1:17]
@@ -322,10 +263,10 @@ objrel <- cbind(objrel, obj.5.dims)
 
 # CREATE GRAPHICS -------------------------------------------------------
 
-# code for creating tables of associations for a given dimension with 
+# Code for creating tables of associations for a given dimension with 
 # knitr. Tables are uneven, so they are cumbersome to create automatically
-# with markdown, pandco, etc. I use tableGrobs in the gridExtra to create
-# table graphics 
+# with markdown, pandoc, etc. I use tableGrobs in the gridExtra package to 
+# create table graphics.
 
 # function for vertically justifying the three grobs
 justify <- function(x, hjust="center", vjust="top", draw = FALSE){
@@ -342,28 +283,45 @@ justify <- function(x, hjust="center", vjust="top", draw = FALSE){
 	return(x)
 }
 
-# make the tables of the significant variables 
+# Make the tables of the significant variables from a dimdesc object,
 famd.table <- function(dd, widths = c(.5,.2,.5)){
-    # dd is one dimension from a dimdesc() object
-	# make the matrices for the tables (without p values)
-    est1 <- as.matrix(data.frame(correlation = dd[[1]][, 1]))
+  # dd is one dimension from a dimdesc() object
+	# Make the matrices for the tables (without p values).
+  est1 <- as.matrix(data.frame(correlation = dd[[1]][, 1]))
 	rownames(est1) <- rownames(dd[[1]])
 	est2 <- as.matrix(data.frame(R2 = dd[[2]][, 1]))
 	rownames(est2) <- rownames(dd[[2]])
 	est3 <- as.matrix(data.frame(Estimate = dd[[3]][, 1]))
 	rownames(est3) <- rownames(dd[[3]])
   
-    # make tableGrobs
-    a <- justify(tableGrob(round(est1, 3), theme = ttheme_minimal(), widths = unit(c(1), "mm")))
-    b <- justify(tableGrob(round(est2, 3), theme = ttheme_minimal(), widths = unit(c(2), "cm")))
-    d <- justify(tableGrob(round(est3, 3), theme = ttheme_minimal(), widths = unit(c(1), "cm")))
+  # make tableGrobs
+  a <- justify(tableGrob(round(est1, 3), theme = ttheme_minimal(), widths = unit(c(1), "mm")))
+  b <- justify(tableGrob(round(est2, 3), theme = ttheme_minimal(), widths = unit(c(2), "cm")))
+  d <- justify(tableGrob(round(est3, 3), theme = ttheme_minimal(), widths = unit(c(1), "cm")))
 	# plot tables
 	grid.arrange(a, b, d, nrow = 1, widths = widths)
 }
 
-# famd.table(dd1[[1]])
+# For the stylistic factors:
+famd.table(dd1[[1]])
+famd.table(dd1[[2]])
+
+famd.table(dd2[[1]])
+famd.table(dd2[[2]])
+# and so on...
+
+# For the internal factors:
+famd.table(dd3[[1]])
+famd.table(dd3[[2]])
+
+famd.table(dd4[[1]])
+famd.table(dd4[[2]])
+# and so on...
+
 
 # --- Scree plots ---
+
+# Create the scree plots for each FAMD.
 
 ## stylistic factors
 
